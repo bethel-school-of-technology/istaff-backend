52bcbce6df3a886c5bdb647ef1e039d9d112e473
Jrus22 <53241882+Jrus22@users.noreply.github.com>
2020-02-13 17:01:17 -0700

    some route defaults

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/signup', function (req, res, next) {
+  res.render('signup');
+});
+
+router.post('/signup',function (req,res, next){
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login');
+})
+
+router.post('/login', function (req, res, next) {
+});
+
+router.get('/admin', function (req, res, next) {
+});
+
+router.get('/logout', function (req, res, next) {
+  console.log('Logging User Out....');
+  res.cookie('jwt', '', { expires: new Date(0) });
+  console.log('User is Now Logged Out....');
+  res.redirect('/users/login');
+});
+
 module.exports = router;
