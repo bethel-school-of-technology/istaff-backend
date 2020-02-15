a601aece593e0e5d0b3347327db93254386ad282
Joshua <Lyons>
2020-02-15 00:53:18 -0700

    Sequelize Models

---
+++ models/emp.js
@@ -0,0 +1,46 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('emp', {
+    idemp: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true
+    },
+    hireDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    dob: {
+      type: DataTypes.DATEONLY,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    middleName: {
+      type: DataTypes.STRING(45),
+      allowNull: true
+    },
+    lastName: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    active: {
+      type: DataTypes.INTEGER(4),
+      allowNull: false,
+      defaultValue: '1'
+    }
+  }, {
+    tableName: 'emp'
+  });
+};
