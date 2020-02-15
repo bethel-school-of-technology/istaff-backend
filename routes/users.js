8ee947e90246fcc9136de50c388d82bc0175e3a2
Jrus22 <53241882+Jrus22@users.noreply.github.com>
2020-02-14 20:18:03 -0700

    branch off of testing

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,7 @@ router.get('/admin', function (req, res, next) {
 
 router.get('/logout', function (req, res, next) {
   console.log('Logging User Out....');
-  res.cookie('jwt', '', { expires: new Date(0) });
+ // (tokens have not been added) res.cookie('jwt', '', { expires: new Date(0) });
   console.log('User is Now Logged Out....');
   res.redirect('/users/login');
 });
